402a492d682b1ada1f2225947f8ac0a3909ea1a2
taimoorhamza1999 <taimoorhamza1999@gmail.com>
2025-01-16 19:16:47 +0400

    add testing route

--- index.js
+++ index.js
@@ -50,6 +50,10 @@ app.post('/create-checkout-session', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+app.get('/', async (req, res) => {
+  res.status(200).json({ message: "error.message" });
+})
+
 
 const PORT = 5002;
 app.listen(PORT, () => {
